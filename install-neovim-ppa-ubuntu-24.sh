6a2a26960159a8da33bc1fe59dde2439d41c349d
ddxfish <ddxfish@gmail.com>
2025-10-01 22:15:36 -0400

    Update install-neovim-ppa-ubuntu-24.sh
    added vim-plug

--- install-neovim-ppa-ubuntu-24.sh
+++ install-neovim-ppa-ubuntu-24.sh
@@ -1,3 +1,6 @@
 sudo add-apt-repository ppa:neovim-ppa/stable -y
 sudo apt update
 sudo apt install neovim -y
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
